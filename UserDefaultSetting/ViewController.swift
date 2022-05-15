e9eef9c9c166d9ad3ad3da238f95af8fa71f9524
Omar Tharwat <omartharwat518@gmail.com>
2022-05-15 16:26:58 +0200

    Initial Commit

---
+++ UserDefaultSetting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultSetting
+//
+//  Created by Omar Tharwat on 5/15/22.
+//  Copyright © 2022 Omar Tharwat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
